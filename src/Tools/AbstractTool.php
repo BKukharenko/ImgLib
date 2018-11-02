5ee2559f97f1de665290c509878e36c70a849989
BKukharenko <bohdankukharenko@gmail.com>
2018-11-02 19:03:06 +0200

    structure of abstract class AbstractTool

--- src/Tools/AbstractTool.php
+++ src/Tools/AbstractTool.php
@@ -4,5 +4,33 @@ namespace GH\ImgLib\Tools;
 
 abstract class AbstractTool
 {
+    /**
+     * Arguments of tool
+     *
+     * @var array
+     *
+     */
+    public $arguments;
 
+
+    /**
+     * Applies current tool on image
+     *
+     * @param \GH\ImgLib\Image $image
+     *
+     * @return mixed
+     *
+     */
+    abstract public function apply($image);
+
+
+    /**
+     * Creates new tool instance.
+     *
+     * @param array $arguments
+     */
+    public function __construct($arguments)
+    {
+        $this->arguments = $arguments;
+    }
 }
