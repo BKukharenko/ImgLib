03305f92c2e8ab0dfd2ef7b4b79f795b36c2b45c
BKukharenko <bohdankukharenko@gmail.com>
2018-11-02 19:37:22 +0200

    structure of Tools classes

--- src/Tools/Crop.php
+++ src/Tools/Crop.php
@@ -6,4 +6,15 @@ namespace GH\ImgLib\Tools;
 class Crop extends AbstractTool
 {
 
+  /**
+   * Crop image
+   *
+   * @param \GH\ImgLib\Image $image
+   *
+   * @return mixed|void
+   */
+    public function apply($image)
+    {
+        // TODO: Implement apply() method.
+    }
 }
--- src/Tools/Resize.php
+++ src/Tools/Resize.php
@@ -5,4 +5,15 @@ namespace GH\ImgLib\Tools;
 class Resize extends AbstractTool
 {
 
+  /**
+   * Resize image dimensions
+   *
+   * @param \GH\ImgLib\Image $image
+   *
+   * @return mixed|void
+   */
+    public function apply($image)
+    {
+        // TODO: Implement apply() method.
+    }
 }
