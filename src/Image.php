83a7390a4f92fe64ae49329cd722a0bcf7fcb019
BKukharenko <bohdankukharenko@gmail.com>
2018-11-03 16:19:13 +0200

    structure of File Interface and Image class

--- src/Image.php
+++ src/Image.php
@@ -7,4 +7,33 @@ use GH\ImgLib\Interfaces\FileInterface;
 class Image implements FileInterface
 {
 
+  /**
+   *
+   * @inheritdoc
+   *
+   */
+    public function init($data)
+    {
+        // TODO: Implement init() method.
+    }
+
+    /**
+     *
+     * @inheritdoc
+     *
+     */
+    public function checkFormat($data)
+    {
+        // TODO: Implement checkFormat() method.
+    }
+
+    /**
+     *
+     * @inheritdoc
+     *
+     */
+    public function save($path = null)
+    {
+        // TODO: Implement save() method.
+    }
 }
--- src/Interfaces/FileInterface.php
+++ src/Interfaces/FileInterface.php
@@ -5,4 +5,32 @@ namespace GH\ImgLib\Interfaces;
 interface FileInterface
 {
 
+  /**
+   * Initiates image from mixed data
+   *
+   * @param mixed $data
+   *
+   * @return object
+   *
+   */
+    public function init($data);
+
+    /**
+     * Check image format
+     *
+     * @param $data
+     *
+     * @return mixed
+     *
+     */
+    public function checkFormat($data);
+
+    /**
+     * Saves image in filesystem
+     *
+     * @param string $path
+     *
+     * @return \GH\ImgLib\Image
+     */
+    public function save($path = null);
 }
