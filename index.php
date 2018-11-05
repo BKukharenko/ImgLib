71b58c633e44ac86dd3ca3e0d0d992ba1856902f
BKukharenko <bohdankukharenko@gmail.com>
2018-11-05 12:38:19 +0200

    index.php - entry point

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
 <?php
 
+use \GH\ImgLib\Image;
+use \GH\ImgLib\Tools\Crop;
+use \GH\ImgLib\Tools\Resize;
+use \GH\ImgLib\Size;
+
 require_once __DIR__ . '/vendor/autoload.php';
+
+$data = '';
+$newSize = array(250, 250);
+
+$imageSize = new Size();
+
+//Image for crop
+$imageCrop = new Image();
+
+$imageCrop->init($data);
+$imageCrop->checkFormat($data);
+
+$imageCropHeight = $imageSize->getHeight();
+$imageCropWidth = $imageSize->getWidth();
+
+$cropper = new Crop($newSize);
+$cropper->apply($imageCrop);
+
+$imageCrop->save();
+
+//Image for resize
+$imageResize = new Image();
+
+$imageResize->init($data);
+$imageCrop->checkFormat($data);
+
+$resizer = new Resize($newSize);
+$resizer->apply($imageResize);
+
+$imageResize->save();
